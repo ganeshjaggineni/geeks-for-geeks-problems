130473dc5ae142efda853b951cae31c543aa9f64
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-02-22 22:14:22 +0530

    Add files via upload

---
+++ MaximizeToys.java
@@ -0,0 +1,20 @@
+
+
+//User function Template for Java
+
+class Solution{
+    static int toyCount(int N, int K, int arr[])
+    {
+       Arrays.sort(arr);
+       int count = 0;
+       for(int i=0;i<N;i++)
+       {
+           if(arr[i]<=K)
+           {
+               count++;
+               K=K-arr[i];
+           }
+       }
+       return count;
+    }
+}
