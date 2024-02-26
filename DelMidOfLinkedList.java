e88aa820c4e74400f067b4abeacd1d4bc355a638
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-02-26 22:16:45 +0530

    Add files via upload

---
+++ DelMidOfLinkedList.java
@@ -0,0 +1,26 @@
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+    
+        Node slow = head;
+        Node fast = head.next.next;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+    return head;
+    }
+}
