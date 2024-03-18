954a0574f7c747bbe09988223840a65957d407b3
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-03-18 22:22:44 +0530

    Add files via upload

---
+++ IsDivisibleBy3.java
@@ -0,0 +1,19 @@
+
+
+// User function Template for Java
+
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        // code here
+        long sum = 0;
+        for(int i=0;i<N;i++)
+        {
+            sum = sum+arr[i];
+        }
+        if(sum%3==0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
