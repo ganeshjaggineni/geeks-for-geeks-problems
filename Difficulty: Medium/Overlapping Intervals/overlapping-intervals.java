b23d5dc435b5131a98547be5b99e8d7b3790fdd1
GANESH JAGGINENI <121171965+ganeshjaggineni@users.noreply.github.com>
2024-11-28 19:17:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            // The mergeOverlap function now returns a List<int[]>
+            List<int[]> ans = obj.mergeOverlap(arr);
+
+            // Printing the merged arr
+            for (int[] interval : ans) {
+                System.out.print(interval[0] + " " + interval[1] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public List<int[]> mergeOverlap(int[][] arr) {
+        // Code here // Code here
+        
+        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+        // System.out.println(Arrays.deepToString(arr));
+        int n = arr.length;
+        int start = arr[0][0];
+        int maxPrevEnd = arr[0][1];
+        List<int[]> res = new ArrayList<>();
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i][0] <= maxPrevEnd)
+            {
+                maxPrevEnd = Math.max(maxPrevEnd,arr[i][1]);
+            }
+            else
+            {
+                res.add(new int[]{start,maxPrevEnd});
+                start = arr[i][0];
+                maxPrevEnd = arr[i][1];
+            }
+        }
+        res.add(new int[]{start,maxPrevEnd});
+        return res;
+        
+        
+    }
+}
